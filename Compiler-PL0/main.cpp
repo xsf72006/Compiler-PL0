55e43f30a8ed771dcad45984d83c7fdf49294ad0
徐少峰Timmy <xsf72006@gmail.com>
2014-12-25 10:46:45 +0800

    Project init

---
+++ Compiler-PL0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compiler-PL0
+//
+//  Created by TimmyXu on 14/12/25.
+//  Copyright (c) 2014年 TimmyXu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
